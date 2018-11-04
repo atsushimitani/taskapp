adb38d6379a190b13fc85b19587eb7ac5a9cc337
Atsushi Mitani <tech.mitani@gmail.com>
2018-11-04 17:40:34 +0900

    一覧画面と詳細画面の作成、Realmでタスク保存ができるようにする

---
+++ taskapp/InputViewController.swift
@@ -0,0 +1,55 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by 三谷淳史 on 2018/10/29.
+//  Copyright © 2018年 atsushi.mitani. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class InputViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var contentsTextView: UITextView!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var task: Task!
+    let realm = try! Realm()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 背景をタップしたらdismissKeyboardメソッドを呼ぶように設定する
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date
+            self.realm.add(self.task, update: true)
+        }
+        
+        super.viewWillDisappear(true)
+    }
+    
+    @objc func dismissKeyboard() {
+        // キーボードを閉じる
+        view.endEditing(true)
+    }
+}
---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 三谷淳史 on 2018/11/02.
+//  Copyright © 2018年 atsushi.mitani. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    //　管理用ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    // 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     IDをプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
